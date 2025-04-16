bddab2eede475c5e3289c3ed0f69d5c6d42d5ac1
tungbinzzz <tungvan2024@gmail.com>
2025-04-16 16:53:06 +0700

    init enterLottery func

--- src/Lottery.sol
+++ src/Lottery.sol
@@ -1,14 +1,81 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
+// Layout of Contract:
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
 
-contract Counter {
-    uint256 public number;
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
+/// @title Lottery Contract
+/// @author tungbinzzz
+/// @notice User can create a lottery and join a lottery
+/// @dev This contract is used to create and manage lotteries
+
+contract Lottery {
+/*//////////////////////////////////////////////////////////////
+                               ERRORS
+    //////////////////////////////////////////////////////////////*/
+    error Lottery__InvalidEntranceFee();
+    error Lottery__NotOpen();
+
+    /*//////////////////////////////////////////////////////////////
+                               TYPE DECLARATIONS
+    //////////////////////////////////////////////////////////////*/
+    enum LotteryState {
+        OPEN,
+        CLOSED,
+        CALCULATING_WINNER
     }
+ 
+ /*//////////////////////////////////////////////////////////////
+                               STATE VARIABLES
+    //////////////////////////////////////////////////////////////*/
+    LotteryState private s_lotteryState;
+ uint256 private immutable i_entranceFee;
+ uint256 private s_rewardBalance;
+ address[] private s_players;
 
-    function increment() public {
-        number++;
+/*//////////////////////////////////////////////////////////////
+                               EVENTS
+    //////////////////////////////////////////////////////////////*/
+event LotteryEntered(address indexed player, uint256 amount);
+
+/*//////////////////////////////////////////////////////////////
+                                FUNCTION
+    //////////////////////////////////////////////////////////////*/
+ constructor(uint256 entranceFee) {
+     i_entranceFee = entranceFee;
+ }
+
+/*//////////////////////////////////////////////////////////////
+                               PUBLIC FUNCTION
+    //////////////////////////////////////////////////////////////*/
+ function enterLottery() public payable {
+    if(s_lotteryState != LotteryState.OPEN) {
+        revert Lottery__NotOpen();
     }
+    if(msg.value < i_entranceFee) {
+        revert Lottery__InvalidEntranceFee();
+    }
+    s_rewardBalance += msg.value;
+    s_players.push(msg.sender);
+
+    emit LotteryEntered(msg.sender, msg.value);
+ }
+
 }
